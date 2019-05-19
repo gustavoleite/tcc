28b222e68f065cb7a6dc3f96872ffe3fc3ab718f
Gustavo Leite <gustavo.jog@hotmail.com>
2019-05-19 12:58:39 -0300

    setup project

---
+++ app/src/main/java/gustavo/guterres/leite/tcc/feature/HomeActivity.kt
@@ -0,0 +1,19 @@
+package gustavo.guterres.leite.tcc.feature
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+import gustavo.guterres.leite.tcc.R
+
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+    }
+}
