9b9dbceb9e3a7108eac088bab850841c1fd99b24
Gustavo Leite <gustavo.jog@hotmail.com>
2019-06-23 12:53:10 -0300

    added step builder

---
+++ app/src/main/java/gustavo/guterres/leite/tcc/feature/step/StepBuilder.kt
@@ -0,0 +1,19 @@
+package gustavo.guterres.leite.tcc.feature.step
+
+import gustavo.guterres.leite.tcc.data.entity.model.Step
+
+class StepBuilder {
+
+
+    fun getFragmentList(steps: List<Step>, onStepFinished: () -> Unit): List<StepFragment> {
+
+        val fragments = mutableListOf<StepFragment>()
+
+        for (step in steps) {
+            fragments.add(StepFragment.newInstance(step, onStepFinished))
+        }
+
+        return fragments
+    }
+
+}
